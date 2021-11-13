ded1a408338edc3e42d11759d238cf31a794ff98
Alan Espinet <aelluvetaa@gmail.com>
2021-11-13 15:23:10 -0500

    adding partials and styles setup

--- index.js
+++ index.js
@@ -7,15 +7,30 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || '4400';
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
+    defaultLayout: 'main',
+    partialsDir: path.join(__dirname, 'views', 'partials')
 }));
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get('/', (req, res) => { 
+    const templateInfo = {};
+
+    const menu = [
+        { label: "Home", url: '/' },
+        { label: "Portfolio", url: '/#portfolio' }
+    ];
+
+    templateInfo['menu'] = menu;
+
+    res.render('home', { 
+        data: templateInfo,
+        title: 'Full Stack Developer'
+    }); 
 })
 
 app.listen( port, () => { console.log('Server is Running') } );
